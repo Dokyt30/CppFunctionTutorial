aaccc983b1b9e60c2133e21a4cbb1adfbca65338
y <y@a>
2018-05-16 17:34:18 +0900

    sort tutorial add

--- FuncSort.cpp
+++ FuncSort.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
+
+
 
 // ŠÖ”“àƒNƒ‰ƒX
 void sorttest() {
 	struct mginfo {
 		int number;
 		int order;
+		mginfo(int _n, int _o) :number(_n), order(_o) {}
 		void print() {
 			cout << "number:" << number << ", order:" << order << endl;
 		}
-	};
-	mginfo a,b;
-	a.number = 1;
-	a.order = 10;
-	b.number = 2;
-	b.order = 3;
 
-	a.print();
-	b.print();
+		
+	};
+	mginfo ary[3] = { mginfo(4,10), mginfo(3,3), mginfo(1,40) };
+	for (auto i : ary) {
+		i.print();
+	}
+
+	std::sort(ary, std::end(ary), [](const mginfo& a, const mginfo& b) {
+		return a.number < b.number;
+	});
+
+	for (auto i : ary) {
+		i.print();
+	}
+
+
 }
 
 int main()
